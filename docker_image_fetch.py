ce28221c1cf03d3ab864bd0b3bb830dabf36b010
Zoey Garvey <code@zoeyinthe.cloud>
2020-03-22 19:53:15 -0700

    Update dependencies and add authorization

--- docker_image_fetch.py
+++ docker_image_fetch.py
@@ -1,7 +1,8 @@
 import os
-import json
-import optparse
+import json5
+import argparse
 import requests
+import base64
 
 # pulls Docker Images from unauthenticated docker registry api. 
 # and checks for docker misconfigurations. 
@@ -14,28 +15,31 @@ final_list_of_blobs = []
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-parser = optparse.OptionParser()
-parser.add_option('-u', '--url', action="store", dest="url", help="URL Endpoint for Docker Registry API v2. Eg https://IP:Port", default="spam")
-options, args = parser.parse_args()
+parser = argparse.ArgumentParser(description="Download all the things from a docker registry")
+parser.add_argument('-u', '--url', dest="url", help="URL Endpoint for Docker Registry API v2. Eg https://IP:Port", default="spam")
+parser.add_argument('-n', '--user', dest="user", help="Username for basic authentication")
+parser.add_argument('-p', '--password', dest="password", help="Password for basic authentication")
+options = parser.parse_args()
 url = options.url
-
-
+auth = base64.b64encode(options.user + ":" + options.password)
+headers = { 'Authorization': 'Basic ' + auth}
 
 def list_repos():
-	req = requests.get(url+ "/" + apiversion + "/_catalog", verify=False)
-	return json.loads(req.text)["repositories"]
+	req = requests.get(url+ "/" + apiversion + "/_catalog", verify=False, headers=headers)
+	print(req.text)
+	return json5.loads(req.text)["repositories"]
 
 def find_tags(reponame):
-	req = requests.get(url+ "/" + apiversion + "/" + reponame+"/tags/list", verify=False)
+	req = requests.get(url+ "/" + apiversion + "/" + reponame+"/tags/list", verify=False, headers=headers)
 	print "\n"
-	data =  json.loads(req.content)
+	data =  json5.loads(req.content)
 	if "tags" in data:
 		return data["tags"]
 
 
 def list_blobs(reponame,tag):
-	req = requests.get(url+ "/" + apiversion + "/" + reponame+"/manifests/" + tag, verify=False)
-	data = json.loads(req.content)
+	req = requests.get(url+ "/" + apiversion + "/" + reponame+"/manifests/" + tag, verify=False, headers=headers)
+	data = json5.loads(req.content)
 	if "fsLayers" in data:
 		for x in data["fsLayers"]:
 			curr_blob = x['blobSum'].split(":")[1]
@@ -43,7 +47,7 @@ def list_blobs(reponame,tag):
 				final_list_of_blobs.append(curr_blob)
 
 def download_blobs(reponame, blobdigest,dirname):
-	req = requests.get(url+ "/" + apiversion + "/" + reponame +"/blobs/sha256:" + blobdigest, verify=False)
+	req = requests.get(url+ "/" + apiversion + "/" + reponame +"/blobs/sha256:" + blobdigest, verify=False, headers=headers)
 	filename = "%s.tar.gz" % blobdigest
 	with open(dirname + "/" + filename, 'wb') as test:
 		test.write(req.content)
